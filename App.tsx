58c4e054314e192cc663d6b60ba1b510a1f52e54
Ankur <109734325+ankurpunia30@users.noreply.github.com>
2024-05-20 15:11:40 +0500

    completed the project at one level

--- App.tsx
+++ App.tsx
@@ -1,118 +1,357 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, TouchableOpacity, TextInput,Keyboard } from 'react-native'
+import React, { useState } from 'react'
+//form validation
+import * as Yup from 'yup'
+import { Formik } from 'formik'
+//this is the main component of the app
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+//yup is a library for form validation
+//it is used to validate the form fields
+//and there are other uses of yup 
+//like formating the data
+const PasswordSchema=Yup.object().shape({
+  passwordLength:Yup.number()
+  .min(4,'Should be at least 4 characters')
+  .max(16,'Should be at most 16 characters')
+  .required('Length is required')
+})
+export default function App() {
+  //[predefined state,method to update the state]=useState(initial value)
+  //useState is a hook that is used to manage the state of the component
+  //password is the state variable
+  //setPassword is the method that is used to update the state
+  //we will never update the state directly
+  //we will always use the method to update the state
+  const [password,setPassword]=useState('')
+  const [isPasswordGenerated,setIsPasswordGenerated]=useState(false)
+  const [lowerCase,setLowerCase]=useState(true)
+  const [upperCase,setUpperCase]=useState(false)
+  const [number,setNumber]=useState(false)
+  const [specialCharacter,setSpecialCharacter]=useState(false)
+  //treat use state as a variable
+  //and treat the method as a function that is used to update the variable 
+  //this function will be used to generate the password string
+  const generatePasswordString=(passwordLength:number)=>{
+    //
+    let characterList='';
+    const lowerCaseCharacters='abcdefghijklmnopqrstuvwxyz'
+    const upperCaseCharacters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numberCharacters='0123456789'
+    const specialCharacters='!@#$%^&*()_+'
+    if(upperCase){
+      characterList+=upperCaseCharacters
+    }
+    if(lowerCase){
+      characterList+=lowerCaseCharacters
+    }
+    if(number){
+      characterList+=numberCharacters
+    }
+    if(specialCharacter){
+      characterList+=specialCharacters
+    }
+    //this function will be used to generate the password
+    //it will return the password
+    const passwordResult=createPassword(characterList,passwordLength)
+    //this function will update the state of the password
+    setPassword(passwordResult)
+    setIsPasswordGenerated(true)
+    //this function will return the password
+    return password
+  }
+  //this function will be used to generate the password 
+  const createPassword=(characters: string,passwordLength:number)=>{
+    // we will use the characters to generate the password 
+    let result=''
+    for(let i=0;i<passwordLength;i++){
+      //this will generate the random index
+      const charIndex=Math.round(Math.random()*characters.length)
+      //this will add the character to the result string
+      result+=characters.charAt(charIndex)
+    }
+    //this will return the result
+    return result
+  }
+  //this function will be used to update the state of the password 
+  const resetPasswordState=()=>{
+    // this will update the state of the password
+    //as we are resetting the password
+    //we will also reset the state of the password
+    //and the state of the checkboxes
+    setPassword('')
+    setLowerCase(true)
+    setUpperCase(false)
+    setNumber(false)
+    setSpecialCharacter(false)
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    setIsPasswordGenerated(false)
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    //keyboardShouldPersistTaps is used to hide the keyboard when the user taps outside the input field
+    <ScrollView keyboardShouldPersistTaps="always">
+            <SafeAreaView style={styles.appContainer}>
+        
+        <View style={styles.formContainer}>
+         
+          <Text style={styles.title}>Password Generator</Text>
+          <Formik
+          //initialValues is used to set the initial values of the form fields
+       initialValues={{passwordLength: '' }}
+       //validationSchema is used to set the validation schema and the validation rules as well as the error messages
+       validationSchema={PasswordSchema}
+       //onSubmit is used to handle the form submission and in this case, it will generate the password string which will be displayed to the user
+        onSubmit={values=>{
+          console.log(values);
+          //this function will generate the password string we are passing the password length to the function through the values object and converting the password length to a number
+          generatePasswordString(Number(values.passwordLength))
+          //this function will hide the keyboard when the user taps outside the input field 
+          Keyboard.dismiss();
+        }}
+      
+     >
+       {({
+        //these are the formik props that are used to manage the form fields
+        //values is used to get the values of the form fields
+         values,
+         //errors is used to get the errors of the form fields
+         errors,
+         //touched is used to get the touched state of the form fields adn it is used to check if the form field is touched or not
+         touched,
+         //isValid is used to check if the form is valid or not because we have set the validation schema and the validation rules
+         isValid,
+          //handleChange is used to handle the change in the form fields and it is used to update the form fields
+         handleChange,
+         //handleSubmit is used to handle the form submission and it is used to submit the form
+         handleSubmit,
+         //handleReset is used to handle the reset of the form fields and it is used to reset the form fields
+         handleReset,
+         //isSubmitting is used to check if the form is submitting or not and it is used to check the submitting state of the form
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+        <>
+        <View style={styles.inputWrapper}>
+          <View style={styles.inputColumn}>
+            <Text style={styles.heading}>Password Length</Text>
+            {touched.passwordLength && errors.passwordLength && (
+              <Text style={styles.errorText}>
+                {errors.passwordLength}
+              </Text>
+            )}
+          
+          
+          </View>
+          <TextInput 
+          style={styles.inputStyle}
+          value={values.passwordLength}
+          onChangeText={handleChange('passwordLength')}
+          placeholder='Eg. 8'
+          keyboardType='numeric'
+          />
+        
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+                <View style={styles.inputWrapper}>
+                   
+                    <Text style={styles.heading}>Include lowercase</Text>
+                      <View>
+                      <BouncyCheckbox
+                      disableBuiltInState
+                      isChecked={lowerCase}
+                      onPress={() => setLowerCase(!lowerCase)}
+                      fillColor="#29AB87"
+                      />  
+                      </View>
+                      
+                  </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.heading}>Include Uppercase letters</Text>
+                  <View>
+                    <BouncyCheckbox
+                      disableBuiltInState
+                      isChecked={upperCase}
+                      onPress={() => setUpperCase(!upperCase)}
+                      fillColor="#FED85D"
+                    />
+                  </View>
+                  
+                </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.heading}>Include Numbers</Text>
+                  <View>
+                  <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={number}
+                    onPress={() => setNumber(!number)}
+                    fillColor="#C9A0DC"
+                  />
+                </View>  
+                </View>
+                <View style={styles.inputWrapper}>
+                  <Text style={styles.heading}>Include Symbols</Text>
+                    <View>
+                    <BouncyCheckbox
+                    disableBuiltInState
+                    isChecked={specialCharacter}
+                    onPress={() => setSpecialCharacter(!specialCharacter)}
+                    fillColor="#FC80A5"
+                  />
+
+                    </View>
+                  
+                </View>
+              <View style={styles.formActions}>
+                  <TouchableOpacity
+                  disabled={!isValid} 
+            style={styles.primaryBtn}
+        onPress={()=>handleSubmit()}
+        >
+          <Text style={styles.primaryBtnTxt}>
+          Generate Password
+          </Text>
+          </TouchableOpacity>
+        
+        <TouchableOpacity
+        style={styles.secondaryBtn}
+        onPress={()=>{
+          handleReset();
+          resetPasswordState();
+        }}
+        >
+          <Text  style={styles.secondaryBtnTxt}>Reset</Text>
+          </TouchableOpacity>
+        </View>
+        
+        </>
+       )}
+     </Formik>
+     {isPasswordGenerated ?(
+        <View style={[styles.card,styles.cardElevated]}>
+        <Text style={styles.subTitle}>Result:</Text>
+        <Text style={styles.description}>Long Press to copy</Text>
+        <Text selectable={true} style={styles.generatedPassword}>{password}</Text>
+
+        
+      </View>
+     ):null}
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  )
 }
+//this is the stylesheet of the app 
+//it is used to style the app
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  //appContainer is the main container of the app 
+  appContainer: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
+  //formContainer is the container of the form and the form fields
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  //title is the title of the app which is displayed at the top of the app
+  title: {
+    fontSize: 32,
     fontWeight: '600',
+    marginBottom: 15,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  //subTitle is the sub title of the app which is displayed below the title
+  subTitle: {
+    fontSize: 26,
+    color: '#16213e',
+    fontWeight: '600',
+    marginBottom: 2,
   },
-  highlight: {
+  description: {
+    color: '#758283',
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+    fontWeight: '600',
+
+  },
+  inputWrapper: {
+    marginBottom: 12,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  inputColumn: {
+    
+    flexDirection: 'column',
+  },
+  inputStyle: {
+    padding: 8,
+    width: '30%',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#758283',
+    color: 'white',
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#ff0d10',
+  },
+  formActions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#28A745',
+  },
+  primaryBtnTxt: {
+    color: '#fff',
+    textAlign: 'center',
     fontWeight: '700',
   },
-});
+  secondaryBtn: {
+    width: 120,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    borderRadius: 8,
 
-export default App;
+    marginHorizontal: 8,
+    backgroundColor: '#880808',
+  },
+  secondaryBtnTxt: {
+    textAlign: 'center',
+    color:'#FFFFFF',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  card: {
+    backgroundColor: '#f0f0f0',
+    marginVertical: 12,
+
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: '#ffffff',
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color:'#16213e'
+  },
+});
--- README.md
+++ README.md
@@ -1,79 +1,34 @@
-This is a new [**React Native**](https://reactnative.dev) project, bootstrapped using [`@react-native-community/cli`](https://github.com/react-native-community/cli).
+# React Native Password Generator
 
-# Getting Started
+This is a React Native application that allows users to generate random passwords based on their preferences.
 
->**Note**: Make sure you have completed the [React Native - Environment Setup](https://reactnative.dev/docs/environment-setup) instructions till "Creating a new application" step, before proceeding.
+## Features
 
-## Step 1: Start the Metro Server
+- User can specify the length of the password.
+- User can choose to include lowercase letters, uppercase letters, numbers, and special characters in the password.
+- The application uses Formik for form handling and Yup for form validation.
+- The application uses React Native's BouncyCheckbox component for the checkboxes in the form.
+- The application uses React Native's TouchableOpacity component for the buttons in the form.
+- The application uses React Native's TextInput component for the input field in the form.
+- The application uses React Native's Text component to display text in the UI.
+- The application uses React Native's ScrollView and SafeAreaView components to ensure that the application's UI is displayed correctly on all devices.
 
-First, you will need to start **Metro**, the JavaScript _bundler_ that ships _with_ React Native.
+## Installation
 
-To start Metro, run the following command from the _root_ of your React Native project:
+To install the application, follow these steps:
 
-```bash
-# using npm
-npm start
+1. Clone the repository: `git clone https://github.com/yourusername/.git`
+2. Install the dependencies: `npm install`
+3. Start the application: `npm start`
 
-# OR using Yarn
-yarn start
-```
+## Usage
 
-## Step 2: Start your Application
+To use the application, fill out the form with your password preferences and click the "Generate" button. The generated password will be displayed below the form.
 
-Let Metro Bundler run in its _own_ terminal. Open a _new_ terminal from the _root_ of your React Native project. Run the following command to start your _Android_ or _iOS_ app:
+## Contributing
 
-### For Android
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
-```bash
-# using npm
-npm run android
+## License
 
-# OR using Yarn
-yarn android
-```
-
-### For iOS
-
-```bash
-# using npm
-npm run ios
-
-# OR using Yarn
-yarn ios
-```
-
-If everything is set up _correctly_, you should see your new app running in your _Android Emulator_ or _iOS Simulator_ shortly provided you have set up your emulator/simulator correctly.
-
-This is one way to run your app — you can also run it directly from within Android Studio and Xcode respectively.
-
-## Step 3: Modifying your App
-
-Now that you have successfully run the app, let's modify it.
-
-1. Open `App.tsx` in your text editor of choice and edit some lines.
-2. For **Android**: Press the <kbd>R</kbd> key twice or select **"Reload"** from the **Developer Menu** (<kbd>Ctrl</kbd> + <kbd>M</kbd> (on Window and Linux) or <kbd>Cmd ⌘</kbd> + <kbd>M</kbd> (on macOS)) to see your changes!
-
-   For **iOS**: Hit <kbd>Cmd ⌘</kbd> + <kbd>R</kbd> in your iOS Simulator to reload the app and see your changes!
-
-## Congratulations! :tada:
-
-You've successfully run and modified your React Native App. :partying_face:
-
-### Now what?
-
-- If you want to add this new React Native code to an existing application, check out the [Integration guide](https://reactnative.dev/docs/integration-with-existing-apps).
-- If you're curious to learn more about React Native, check out the [Introduction to React Native](https://reactnative.dev/docs/getting-started).
-
-# Troubleshooting
-
-If you can't get this to work, see the [Troubleshooting](https://reactnative.dev/docs/troubleshooting) page.
-
-# Learn More
-
-To learn more about React Native, take a look at the following resources:
-
-- [React Native Website](https://reactnative.dev) - learn more about React Native.
-- [Getting Started](https://reactnative.dev/docs/environment-setup) - an **overview** of React Native and how setup your environment.
-- [Learn the Basics](https://reactnative.dev/docs/getting-started) - a **guided tour** of the React Native **basics**.
-- [Blog](https://reactnative.dev/blog) - read the latest official React Native **Blog** posts.
-- [`@facebook/react-native`](https://github.com/facebook/react-native) - the Open Source; GitHub **repository** for React Native.
+This project is licensed under the [MIT License](LICENSE).
